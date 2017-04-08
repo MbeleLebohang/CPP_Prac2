5bf61e15bc2aa34a841ed89eaba97bbfa62a4814
Mbele Lebohang <micheallm95@gmail.com>
2017-04-08 22:02:01 +0200

    added the read method.

--- VolImage.cpp
+++ VolImage.cpp
@@ -48,19 +48,25 @@ bool VolImage::readImages(const string baseName){
     for(int slice = 0; slice < slice_count; slice++){
         /* All the slices with the baseName. */
         ifstream binary_file (baseName + to_string(slice) + ".raw", ios::in|ios::binary);
-        u_char** image_height_array;
+
+        u_char** image_height_array = new u_char*[height];     //column - place to put all rows
         for(int h = 0; h < height; h++){
             /* All the height levels(row) in the image. */
 
-            u_char* image_width_array;
+            image_height_array[h] = new u_char[width];        // row - place to put all chars
+
+            char* image_width_array = new char[width];
             binary_file.read(image_width_array, width);     //read the whole width of bytes.
 
             for(int w = 0; w < width; w++){
                 /* Each column (width piece). */
-                cout << (unsigned int) image_width_array[w] << " ";
-                binary_file.close();
-                return 0;                    
-            }   
+                image_height_array[h][w] = image_width_array[w];
+            } 
+            
+            delete[] image_width_array;
+            slices.push_back(image_height_array);
+            binary_file.close();
+            return 0;      
         }
     }
 
