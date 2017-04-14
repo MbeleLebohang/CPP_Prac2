b3e8781705cfa587cbaeef1227477e7098c8d4d3
Mbele Lebohang <micheallm95@gmail.com>
2017-04-14 18:53:32 +0200

    driver class can read all necessary cmd args.

--- main.cpp
+++ main.cpp
@@ -27,24 +27,46 @@ int main(int argc, char** argv) {
     VolImage volume;
     volume.readImages(image_base);
     
+    if(image_base.substr(0,1) == "-"){
+        /* Check if the first argument is not a flag.*/
+        cout << "Unknown flag. Please read README.md file." << endl;
+        exit(0);
+    }
+    
     // check if a parameter was given for the name of the log file to use
     if(argc == 6){
-        cout << "compute a difference map between images " 
-        << argv[3] << " and " << argv[4] << 
-        " and write this out to " << argv[5] << " file." << endl;
-    }
-    else if (argc == 5) {
-        if(image_base.substr(0,1) != "-"){
-            cout << "extract and write the slice with number " << argv[3] << " and write this out to " << argv[4] << " file." << endl;
+        if(string(argv[2]) == "-d"){
+            
+            cout << "Computing the difference in image " << argv[3] << " and "<< argv[4] << endl;
+            volume.diffmap(stoi(argv[3]), stoi(argv[4]), argv[5]);
+            cout << "Done. Open " << argv[5] << ".raw using viewer.py to see the results."<<endl; 
         }
         else{
-            cout << "Unknown flag " << argv[1] << ". Please read README.md file." << endl;
+            cout << "Unknown flag " << argv[2] << ". Please read README.md file." << endl;
         }
     }
-    else if (argc == 1)
+    else if (argc == 5) {
+        
+        if(string(argv[2]) == "-i"){
+            
+            cout << "Extracting slice number... " << argv[3] << endl;
+            volume.extract(stoi(argv[3]), argv[4]);
+            cout << "Done. Open " << argv[4] << ".raw using viewer.py to see the results."<<endl; 
+        }
+        else if(string(argv[2]) == "-g"){
+            
+            cout << "Extracting row " << argv[3] << " of all slices..."<< endl;
+            volume.extraCredit(stoi(argv[3]), argv[4]);
+            cout << "Done. Open " << argv[4] << ".raw using viewer.py to see the results."<<endl; 
+        }
+        else{
+            cout << "Unknown flag " << argv[2] << ". Please read README.md file." << endl;
+        }
+    }
+    else if (argc == 2)
     {
-        cout << "Number of images: " << 122 <<endl;
-        cout << "Number of bytes required: " << 122*8 <<endl;
+        cout << "Number of images: " << volume.getSliceCount() <<endl;
+        cout << "Number of bytes required: " << volume.volImageSize() <<endl;
     }
     else{
         cout << "Invalid number of arguments. Please read README.md file." <<endl;
